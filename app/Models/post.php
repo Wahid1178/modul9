e54d82bf9d18475cf39badcfd50eaab8f1e5b76d
Nama Kamu <emailkamu@example.com>
2025-12-09 13:30:36 +0700

    Second commit

--- app/Models/post.php
+++ app/Models/post.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class post extends Model
 {
-    protected $fillable = ['title', 'author', 'article'];
+    protected $fillable = ['title', 'author', 'article','image'];
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LokasiController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,20 @@ use App\Http\Controllers\LokasiController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+route::post('/register', [AuthController::class, 'register']);
+route::post('/login', [AuthController::class, 'login']);
+
+route::middleware('auth:sanctum')->group(function () {
+    route::post('/logout', [AuthController::class, 'logout']);
+    route::get('/me', [AuthController::class, 'me']);
+    
+    route::get('/posts', [PostController::class, 'index']);
+    route::post('/posts', [PostController::class, 'store']);
+    route::get('/posts/{id}', [PostController::class, 'show']);
+    route::put('/posts/{id}', [PostController::class, 'update']);
+    route::delete('/posts/{id}', [PostController::class, 'destroy']);
+});
+
 
 route::get('/posts', [PostController::class, 'index']);
 route::post('/posts', [PostController::class, 'store']);
